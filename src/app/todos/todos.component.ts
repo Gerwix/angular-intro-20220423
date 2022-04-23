44ce4d6696661bb24dc38652949713adc5cfedc2
Gerwix <mateuszgerwazik@gmail.com>
2022-04-23 12:27:42 +0200

    add ngif ngfor

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { Todo } from './todo';
+import {Component, OnInit} from '@angular/core';
+import {Todo} from './todo';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  styleUrls: ['./todos.component.scss'],
 })
 export class TodosComponent implements OnInit {
-  public todos: Todo[] = []
+  public todos: Todo[] = [];
   public text: string = 'text';
 
   public clear() {
     this.text = '';
   }
-  constructor() { }
-
-  ngOnInit(): void {
+  public addTodo() {
+    const todo: Todo = {
+      text: this.text,
+      created: Date.now(),
+      done: false,
+      priority: 1,
+    };
+    this.todos.push(todo);
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
